eb68b2fea5c21ecd4bf195efef61132d0ca3e19b
Devon Bull <dbull@live.com>
2017-03-27 22:48:43 -0600

    Implemented while loop, seems to be a problem evaluating index 0 and 1

--- scripts/bubbleSort.js
+++ scripts/bubbleSort.js
@@ -1,18 +1,28 @@
 let bubbleSort = (array) => {
-  // let sortedArray = []
+  var count = 0
 
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > array[i + 1]) {
-      let temp = array[i]
+  while (count == 0) {
+    for (let i = 0; i < array.length - 1; i++) {
+      count = 0
 
-      array[i] = array[i + 1]
-      array[i + 1] = temp
+      if (array[i] > array[i + 1]) {
+        debugger
+        let temp = array[i]
+
+        array[i] = array[i + 1]
+        array[i + 1] = temp
+        count ++
+      }
     }
-
+    if (count != 0) {
+      count = 0
+    } else {
+      return array
+    }
   }
-
-  return array
 }
+
+bubbleSort([45, 22, 10, 12, 1])
 
 
 
