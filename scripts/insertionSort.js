3c15de53d14d9cd15fcc18f08f374b94cf11eab6
Devon Bull <dbull@live.com>
2017-03-27 21:34:22 -0600

    Add locus, insertionSort file and instertionSort-test

---
+++ scripts/insertionSort.js
@@ -0,0 +1,34 @@
+let insertionSort = (array) => {
+  let sortedArray = []
+
+
+
+
+
+
+
+
+  return sortedArray
+}
+
+
+module.exports = insertionSort
+
+
+
+
+// for (let i = 0; i < sortedArray.length; i++) {
+//   if (array[i] > !sortedArray[i]) {
+//     sortedArray.push(array[i])
+//   } else if (array[i] == !sortedArray[i]) {
+//
+//     sortedArray.splice(i, 0, array[i])
+//   }
+//
+// }
+
+
+
+// } else if (array[j] == !sortedArray[i]) {
+//   sortedArray.splice(i, 0, array[j])
+//   j++
--- tests/bubbleSort-test.js
+++ tests/bubbleSort-test.js
@@ -15,6 +15,6 @@ describe('Bubble Sort Tests', () => {
   // })
 
   it('should organize the elements by value', () => {
-    assert.deepEqual(bubbleSort([2, 1, 4]), [1, 2, 4])
+    assert.deepEqual(bubbleSort([2, 1, 4, 7, 0, 3]), [0, 1, 2, 3, 4, 7])
   })
 })
